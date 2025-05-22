15ee817f7781ca23d7fc1900f2f36bfe534da5b0
Pranjal <pranjal10092005@gmail.com>
2025-05-22 21:15:21 +0530

    Changed the Button Properties

--- frontend/src/pages/Products.jsx
+++ frontend/src/pages/Products.jsx
@@ -76,7 +76,7 @@ const Products = () => {
             </div>
             <button
               onClick={() => handleAddToCart(product)}
-              className="mt-4 w-full py-2 bg-green-500 text-white rounded"
+              className="mt-4 w-full py-2 bg-blue-500 text-white rounded hover:bg-blue-700 transition duration-700 cursor-pointer"
             >
               Add to Cart
             </button>
